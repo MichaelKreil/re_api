e6a699f743b57635ec1b6972d87d6fdfade50375
Micho <github@michael-kreil.de>
2013-11-24 20:31:59 +0100

    also export locations

--- convert/modules/importer_rp13.js
+++ convert/modules/importer_rp13.js
@@ -3,14 +3,29 @@ var fs = require('fs');
 exports.create = function (path) {
 	var result = {
 		schedule:[],
-		speakers:[]
+		speakers:[],
+		locations:[]
 	};
+
+	var locations = {};
 
 	var data = JSON.parse(fs.readFileSync(path+'/rp13-schedule.json', 'utf8'));
 	data.schedule.day.forEach(function (day) {
 		var dayNo = parseInt(day.index, 10);
 		
 		day.room.forEach(function (room) {
+			var location;
+			if (locations[room.name] === undefined) {
+				location = {
+					name: room.name,
+					id: room.name,
+					sessions:[]
+				}
+				locations[room.name] = location;
+				result.locations.push(location);
+			} else {
+				location = locations[room.name];
+			}
 			
 			room.event.forEach(function (event) {
 				speakers = event.persons.person;
@@ -28,8 +43,8 @@ exports.create = function (path) {
 					}
 				});
 
-				var item = {
-					id: parseInt(event.id, 10),
+				var session = {
+					id: ''+parseInt(event.id, 10),
 					lastmodified: new Date('2013-05-03'),
 					day: dayNo,
 					date: new Date(day.date),
@@ -45,13 +60,24 @@ exports.create = function (path) {
 					speakers: speakers
 				};
 
-				item.end = new Date(item.start.getTime() + item.duration*60000);
-				result.schedule.push(item);
-			})
-		})
+				session.end = new Date(session.start.getTime() + session.duration*60000);
+				result.schedule.push(session);
+
+				location.sessions.push({
+					title:session.title,
+					id:session.id,
+					start:session.start,
+					end:session.end
+				});
+			});
+		});
 	});
 
-	result.locations = [];
+	result.locations.forEach(function (location) {
+		location.sessions.sort(function (a, b) {
+			return a.start - b.start;
+		});
+	});
 
 	return result;
 }
